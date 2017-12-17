20c9c9ff52d7af9452066e60c725f878270e84fe
Gamagoori <tyler.sand12@gmail.com>
2017-12-17 12:43:23 -0800

    Initial commit

---
+++ ex.R
@@ -0,0 +1,10 @@
+# My first program in R Programming 
+new.printf <- function(a){
+    #a <- 'Start and end with single quote' 
+	if(a == '%d'){
+	 print('there is a %d');
+	}
+    print(a) 
+
+}
+new.printf('%d' );
